20b14f067528df3e2a974b86b6ba2ba62312cdb1
JokerJ <kjh5835@gmail.com>
2025-06-29 19:14:48 +0900

    Heap 완료

---
+++ Ch1/Heap/Heap.ts
@@ -0,0 +1,11 @@
+import { Heap } from "./HeapType";
+
+const heap = new Heap();
+heap.insert(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
+
+console.log(heap.heap);
---
+++ Ch1/Heap/HeapType.ts
@@ -0,0 +1,92 @@
+import { log } from "console";
+
+export class Heap {
+    heap: number[];
+
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(data: number) {
+        this.heap.push(data);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] > this.heap[parentIndex]) {
+                [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    pop() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        const root = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        this.heapifyDown();
+        return root;
+    }
+
+    heapifyDown() {
+        let index = 0;
+        while (index < this.heap.length) {
+            const leftChildIndex = index * 2 + 1;
+            const rightChildIndex = index * 2 + 2;
+
+            if (leftChildIndex >= this.heap.length) {
+                break;
+            }
+
+            const leftChild = this.heap[leftChildIndex] || 0;
+            const rightChild = this.heap[rightChildIndex] || 0;
+
+            if (rightChildIndex >= this.heap.length) {
+                if (leftChild > this.heap[index]) {
+                    [this.heap[index], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[index]];
+                    index = leftChildIndex;
+                }
+                break;
+            }
+
+            if (leftChild > rightChild && leftChild > this.heap[index]) {
+                [this.heap[index], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[index]];
+                index = leftChildIndex;
+            } else if (rightChild > leftChild && rightChild > this.heap[index]) {
+                [this.heap[index], this.heap[rightChildIndex]] = [this.heap[rightChildIndex], this.heap[index]];
+                index = rightChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+let heap = new Heap();
+heap.insert(4);
+heap.insert(3);
+heap.insert(10);
+heap.insert(19);
+heap.insert(1);
+heap.insert(2);
+heap.insert(13);
+heap.insert(11);
+heap.insert(12);
+heap.insert(14);
+heap.insert(15);
+heap.insert(16);
+heap.insert(17);
+heap.insert(18);
+heap.insert(20);
+
+console.log(heap.pop());
+console.log(heap.pop());
